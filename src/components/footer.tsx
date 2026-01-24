905dde9b3cfbdcd16332bb73603fe90030f51547
Mustafa Güler <mustafa.guler@yandex.com.tr>
2026-01-24 19:23:53 +0200

    refactor(footer): simplify return statement by removing unnecessary line breaks

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,5 @@
 export default function Footer() {
   const currentYear = new Date().getFullYear();
-  
-  return (
-    <div className="flex justify-center items-center mt-auto border-black/10 border-t h-[40px] px-5 text-sm text-muted-foreground">
-      © {currentYear} ITED Technology LTD. All rights reserved.
-    </div>
-  );
+
+  return <div className="flex justify-center items-center mt-auto border-black/10 border-t h-[40px] px-5 text-sm text-muted-foreground">© {currentYear} ITED Technology LTD. All rights reserved.</div>;
 }
